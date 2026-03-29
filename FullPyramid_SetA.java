fa2858bfb6537935dc906ff31c8a774d7d042dc5
Apeksha2104 <apekshagediya01@gmail.com>
2026-03-29 12:23:32 +0530

    Modified partner code for Practical 4 Set A

--- FullPyramid_SetA.java
+++ FullPyramid_SetA.java
@@ -1,19 +1,23 @@
-public class FullPyramid4thB {
+public class FullPyramid_SetA {
 
     public static void main(String[] args) {
 
         int n = 5;
 
-        // -------- Pattern 1: Centered Star Pyramid --------
         for (int i = 1; i <= n; i++) {
 
+            // print spaces
             for (int s = 1; s <= n - i; s++) {
                 System.out.print(" ");
             }
 
+            // print stars
             for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
             }
 
+            // move to next line
             System.out.println();
         }
+    }
+}
